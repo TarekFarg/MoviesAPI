70e9f6e36a9a05640c30b99f160a91b0229699e5
TarekFarg <tm747632@gmail.com>
2025-08-22 23:25:19 +0300

    Add Genre model and Movie model

--- MoviesAPI/obj/Debug/net9.0/MoviesAPI.AssemblyInfo.cs
+++ MoviesAPI/obj/Debug/net9.0/MoviesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da78ec3975ae959ef58ef87cd9f277aa8cd67c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoviesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
